92bfbd7a846add772b258ab834c34e3b69de34c9
David Bui <david@dtbui.com>
2015-04-05 18:44:28 -0700

    made pretty

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', { root: __dirname });
